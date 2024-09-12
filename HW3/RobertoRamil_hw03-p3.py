2979209c22127b40d5a9d2f962645c16dba1b7f0
Rockfallon <rramil@csus.edu>
2024-09-12 11:12:46 -0700

    in p3 a, b are done just need to do the sum and output to the outfile

--- HW3/RobertoRamil_hw03-p3.py
+++ HW3/RobertoRamil_hw03-p3.py
@@ -1,5 +1,30 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 x, y, z = np.loadtxt("HW3\hw03-data.txt", delimiter=' ', skiprows= 3, usecols={1, 2, 4}, unpack= True)
 
-print(x)
+plt.figure("A")
+
+
+plt.plot(x, y,
+         linestyle = "-",
+         label = "f(x)",
+         color = 'green'
+    
+)
+plt.plot(x, z,
+         linestyle = "-",
+         label = "f(z)",
+         color = 'blue'
+    
+)
+plt.plot(x, z/y,
+         linestyle = "-",
+         label = "f(z/y)",
+         color = 'red',
+)
+
+plt.legend()
+plt.title("Curves based on data.txt")
+plt.savefig("RobertoRamil_hw03-p3_fig.pdf")
+plt.show()
