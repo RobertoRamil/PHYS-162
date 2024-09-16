08867d6d178c5d74364636c5f6f7189a63e8d7c3
Rockfallon <rramil@csus.edu>
2024-09-16 09:57:35 -0700

    Finished HW3 Fully Have the figure and output text that was edited or created.

--- HW3/RobertoRamil_hw03-p3.py
+++ HW3/RobertoRamil_hw03-p3.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x, y, z = np.loadtxt("HW3\hw03-data.txt", delimiter=' ', skiprows= 3, usecols={1, 2, 4}, unpack= True)
+x, y, z = np.loadtxt("hw03-data.txt", delimiter=' ', skiprows= 3, usecols={1, 2, 4}, unpack= True)
 
 plt.figure("A")
 
@@ -23,6 +23,19 @@ plt.plot(x, z/y,
          label = "f(z/y)",
          color = 'red',
 )
+sumx, sumy, sumz = sum(x), sum(y), sum(z)
+avgx, avgy, avgz = sumx/len(x), sumy/len(y), sumz/len(z)
+
+out1 = f"Sum of Array x is {sumx:.2f}, average = {avgx:.3f}\n"
+out2 = f"Sum of Array y is {sumy:.2f}, average = {avgy:.3f}\n"
+out3 = f"Sum of Array z is {sumz:.2f}, average = {avgz:.3f}\n"
+
+
+
+with open("out-p3.txt", 'w') as file:
+    file.write(out1)
+    file.write(out2)
+    file.write(out3)
 
 plt.legend()
 plt.title("Curves based on data.txt")
