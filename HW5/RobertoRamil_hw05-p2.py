4f80fe8f31daa514ea82f5de7ba9ba2e14d77f63
Rockfallon <rramil@csus.edu>
2024-10-02 12:46:59 -0700

    started by reading in the numbers from the 3 txt files

---
+++ HW5/RobertoRamil_hw05-p2.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load data from text files
+x1, y1 = np.loadtxt('hw05-data1.txt', delimiter= ' ', unpack= True)
+x2, y2 = np.loadtxt('hw05-data2.txt', delimiter= ' ', unpack= True)
+x3, y3 = np.loadtxt('hw05-data3.txt', delimiter= ' ', unpack= True)
+
+
+
